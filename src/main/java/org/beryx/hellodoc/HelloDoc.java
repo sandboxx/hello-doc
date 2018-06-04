3aead53360b584eaf30e4f6755856fa5d14ce738
Serban Iordache <iordache@hotmail.com>
2018-06-04 16:10:41 +0200

    release 1.0.0

--- src/main/java/org/beryx/hellodoc/HelloDoc.java
+++ src/main/java/org/beryx/hellodoc/HelloDoc.java
@@ -2,6 +2,6 @@ package org.beryx.hellodoc;
 
 public class HelloDoc {
     public static void main(String[] args) {
-        System.out.println("Hello from HelloDoc!");
+        System.out.println("Hello, Doc!");
     }
 }
